d875ab47939f2c48eda6ac45d2982c065dc10ac3
kelvin165 <kelvin.mendes12345@gmail.com>
2021-09-09 01:11:53 -0300

    Conclusão parte 1 e 2 de Kotlin

---
+++ Primeira.aula-Kotlin/src/Primeira/aula-kotlin.kt
@@ -0,0 +1,19 @@
+fun main(argumentos : Array<String>) {
+
+    if (argumentos.isEmpty()){
+        println("Favor informar um argumento valido")
+        return
+    }
+
+    val numero = argumentos[0].toIntOrNull()
+
+    if (numero == null){
+        println("o Valor informado não é um número válido")
+        return
+    }
+    println("o número digitado foi: $numero")
+
+    var soma = 0
+    soma = numero + numero
+    println("A soma de número + número = $soma")
+}
