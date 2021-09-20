e7c12a115b516b3e0715e5372a2843a9f03eab4b
kelvin165 <kelvin.mendes12345@gmail.com>
2021-09-20 13:12:22 -0300

    Cauculadora parte 3-4 da aula de kotlin

---
+++ Primeira.aula-Kotlin/src/Calculadora.kt
@@ -0,0 +1,41 @@
+import java.math.BigDecimal
+
+fun calculaoperação (primeiroNumero: BigDecimal, operador: String , segundoNumero: BigDecimal) : BigDecimal{
+    when(operador){
+        "+"-> return primeiroNumero + segundoNumero
+        "-"-> return primeiroNumero - segundoNumero
+        "*"-> return primeiroNumero * segundoNumero
+        "/"-> return primeiroNumero / segundoNumero
+        "%"-> return primeiroNumero % segundoNumero
+        else -> return BigDecimal.ZERO
+    }
+}
+
+fun main() {
+
+    println("Digite um número válido")
+    val primeiroNumero = readLine()?.toBigDecimalOrNull()
+
+    if(primeiroNumero == null){
+        println("Primeiro numero informado não é válido")
+        return
+    }
+
+    println("Digite um operador valido ( +,-,*,/,%")
+    val operador = readLine()
+
+    if (operador.isNullOrEmpty()|| operador.isNullOrBlank() ||
+        (operador != null && operador != "+" && operador != "-" && operador != "*" && operador !="/" &&operador !="%" )){
+        println("Operador invalido")
+        return
+    }
+    println("Digite o segundo número válido")
+    val segundoNumero = readLine()?.toBigDecimalOrNull()
+
+    if(segundoNumero == null){
+        println("Segundo numero informado não é válido")
+        return
+        }
+    val resultado = calculaoperação(primeiroNumero, operador, segundoNumero)
+    println("O resultado da sua operaççao foi : $resultado")}
+
